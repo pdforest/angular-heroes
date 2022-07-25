a765cff9ca9ca028dd3b10e607607265fb0d35a0
pdforest <pdforest@gmail.com>
2022-07-24 22:40:02 -0300

    Seccion 14

--- src/app/heroes/pages/agregar/agregar.component.ts
+++ src/app/heroes/pages/agregar/agregar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
+import { Heroe, Publisher } from '../../interfaces/heroe.interface';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-agregar',
@@ -8,9 +12,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AgregarComponent implements OnInit {
 
-  constructor() { }
+  publishers = [
+    {
+      id: 'DC Comics',
+      desc: 'DC - Comics'
+    },
+    {
+      id: 'Marvel Comics',
+      desc: 'Marvel - Comics'
+    }
+  ]
+
+  heroe: Heroe = {
+    superhero: '',
+    alter_ego: '',
+    characters: '',
+    first_appearance: '',
+    publisher: Publisher.DCComics,
+    alt_img: '',
+  }
+
+  constructor(
+    private heroesService: HeroesService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+    ) { }
 
   ngOnInit(): void {
+    this.activatedRoute.params
+    .pipe(
+      switchMap( ({id}) => this.heroesService.getHeroePorId( id ))
+    )
+    .subscribe(
+      heroe => this.heroe = heroe
+    )
   }
+
+  guardar() {
+    if( this.heroe.superhero.trim().length === 0  ) {
+      return;
+    }
+
+    if( this.heroe.id ){
+      // Actualizar
+      console.log("Actualizar ->" + this.heroe.id);
+
+      this.heroesService.actualizarHeroe( this.heroe ).subscribe(
+        resp => console.log("Actualizar " + resp.id)
+      )
+        //.subscribe( heroe => this.mostrarSnakbar('Registro actualizado'));
+
+    } else {
+      // Crear
+      this.heroesService.agregarHeroe( this.heroe ).subscribe( 
+        heroe => {
+          this.router.navigate(['/heroes/editar', heroe.id ]);
+          //this.mostrarSnakbar('Registro creado');
+        })
+    }
+
+  }
+
+  borrarHeroe() {}
 
 }
