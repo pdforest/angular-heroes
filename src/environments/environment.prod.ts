1f2fddbc4b8ad2e6bbf844c6241abe19d2da7e0e
pdforest <pdforest@gmail.com>
2023-06-21 19:42:40 -0300

    fix 1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://localhost:3000'
+  //baseUrl: 'http://localhost:3000'
+  baseUrl: 'https://my-json-server.typicode.com/pdforest/angular-heroes-auth'
 };
